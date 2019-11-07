00502f59aafd49bd38fae7b97a111e6b159c7aee
AnActualEmerald <kevinliger13@gmail.com>
2019-11-07 10:20:50 -0500

    Cleaned up uneeded files - consider this the inital commit for this repo

--- bot.js
+++ bot.js
@@ -3,13 +3,8 @@ var fs = require("fs");
 var Winston = require("winston");
 
 //file locations
-const guildFile = "./guilds.json";
 const configFile = "./config.json";
 const usersFile = "./users.json";
-const varFile = "./commands/cmdVars.json";
-const sayingsFile = "./commands.json";
-const ccFile = "./commands/customCommands.json";
-const starFile = "./starboard.json";
 
 var config = require(configFile);
 var ccF = require(ccFile);
@@ -33,18 +28,8 @@ bot.updateJSON = updateJSON;
 bot.loadCmds = loadCmds;
 
 //initialize bot
-bot.myGuilds = require(guildFile);
-bot.commands = new Discord.Collection();
-bot.coolDowns = new Discord.Collection();
-bot.customComs = JSONtoCollection(require(ccFile));
-bot.globalVar = require(varFile);
-bot.userVars = require(usersFile);
 bot.prefix = config.prefix;
 bot.auth = config.token;
-bot.gr_key = config.goodreads_key;
-bot.gr_secret = config.goodreads_secret;
-bot.sayings = require(sayingsFile);
-bot.ccFile = ccFile;
 
 //set up logging
 bot.logger = Winston.createLogger({
@@ -94,52 +79,10 @@ bot.on("ready", () => {
 var thumbsdown = "👎";
 //end emojis
 
-bot.on("guildMemberAdd", member => {
-    const guildName = member.guild.id;
-    const channelName = bot.myGuilds[guildName].welcomeChannel;
-    const welcomeMsg = bot.myGuilds[guildName].welcomeMessage;
-    var channel;
-
-    try {
-        bot.logger.debug(`guildMemberAdd: cn ${channelName} wm ${welcomeMsg}`);
-        channel = member.guild.channels.find("name", channelName);
-    } catch (err) {
-        bot.logger.error(`Couldn't get channel ${channelName}`);
-        bot.logger.error(err);
-    }
-
-    const result = welcomeMsg.interpolate({ user: member.user });
-
-    if (channel) {
-        channel.send(result);
-    }
-});
-
-bot.on("guildCreate", guild => {
-    bot.myGuilds[guild.id] = {
-        name: guild.name,
-        welcomeChannel: "general",
-        welcomeMessage: "Welcome to the server, <@${user.id}>",
-        ignore: []
-    };
-    updateJSON(guildFile, bot.myGuilds);
-    guild.owner.send(
-        `Hey there, I just joined your server, ${guild.name}! I'm a perhaps not so helpful bot to have around, but I do my best. Use ${bot.prefix}help to learn about my commands. For now,	I'm set to welcome new users in your #general channel if you have one. You can change this and the message I send using ${bot.prefix}welcomechannel and ${bot.prefix}welcomemsg. I look forward to memeing with you!`
-    );
-});
-
 bot.on("message", message => {
     if (message.channel.type === "dm") {
         return handleDM(message);
     }
-
-    if (!bot.userVars[message.author]) {
-        bot.userVars[message.author] = 0;
-    }
-
-    bot.userVars[message.author] = parseInt(bot.userVars[message.author]) + 1;
-
-    updateJSON(usersFile, bot.userVars);
 
     if (message.author.bot) return;
 
@@ -156,8 +99,6 @@ bot.on("message", message => {
             bot.commands.get(cmdName) ||
             bot.commands.find(cmd => cmd.alias && cmd.alias.includes(cmdName));
 
-        if (bot.customComs.has(cmdName))
-            return message.channel.send(bot.customComs.get(cmdName));
         if (!com) {
             return bot.logger.debug(`No command ${cmdName} found`);
         }
@@ -204,31 +145,6 @@ bot.on("message", message => {
                 message.channel.send("Had trouble with that command");
         }
     }
-
-    for (var s in bot.sayings) {
-        var cont = message.content;
-        if (!bot.sayings[s].sens) {
-            cont = cont.toLowerCase();
-        }
-        if (!cont.startsWith(s)) continue;
-        var trigger = cont.substring(0, s.length);
-        bot.logger.silly(`trigger = ${trigger}`);
-        var rest = cont.substring(s.length);
-        bot.logger.silly(`rest = ${rest}`);
-        if (trigger == s) {
-            bot.logger.debug();
-            if (rest) {
-                if (rest.startsWith(" "))
-                    message.channel.send(bot.sayings[s].text);
-            } else {
-                message.channel.send(bot.sayings[s].text);
-            }
-        }
-    }
-
-    updateJSON(varFile, bot.globalVar);
-    updateJSON(sayingsFile, bot.sayings);
-    updateJSON(guildFile, bot.myGuilds);
 });
 
 bot.on("disconnect", event => {
@@ -242,127 +158,6 @@ bot.on("resume", replayed => {
     //may not be necessary but I'm going to do this anyways
     bot.logger.warn("Resuming bot");
     bot.user.setActivity(bot.globalVar.activity);
-});
-
-//event emitter mostly from https://discordjs.guide/popular-topics/reactions.html#emitting-the-event-s-yourself
-const events = { MESSAGE_REACTION_ADD: "messageReactionAdd" };
-bot.on("raw", e => {
-    bot.logger.silly(`Event detected: ${e}`);
-    if (!events.hasOwnProperty(e.t)) return; //check to see if the even is MESSAGE_REACTION_ADD
-    const data = e.d;
-    bot.logger.silly(`Data = ${data}`);
-    const user = bot.users.get(data.user_id);
-    bot.logger.silly(`User = ${user}`);
-    const channel = bot.channels.get(data.channel_id);
-    bot.logger.silly(`Channel = ${channel}`);
-
-    if (channel.messages.has(data.message_id)) return; //prevent double emission
-
-    channel.fetchMessage(data.message_id).then(message => {
-        const emojiKey = data.emoji.id
-            ? `${data.emoji.name}:${data.emoji.id}`
-            : data.emoji.name;
-
-        const reaction = message.reactions.get(emojiKey);
-
-        bot.emit(events[e.t], reaction, user);
-    });
-});
-
-//starboard events
-bot.on("messageReactionAdd", (reaction, user) => {
-    bot.logger.debug(`Reaction detected on ${reaction.message.guild.name}`);
-
-    var server = reaction.message.guild;
-    if (!bot.myGuilds[server.id].starboard_chan) {
-        bot.logger.warn(`Starboard is not set up for ${server.name}`);
-        return;
-    }
-
-    var guildData = bot.myGuilds[server.id];
-    if (guildData.ignore.includes(reaction.message.channel)) {
-        //woops lol
-    }
-
-    if (
-        reaction.count >= guildData.star_lvl &&
-        reaction.emoji == guildData.star_emoji
-    ) {
-        //do starboard stuff
-        var channel = server.channels.find(
-            chan => chan.id === guildData.starboard_chan
-        );
-        bot.logger.debug(`Channel ${channel}`);
-        var author = reaction.message.member;
-        bot.logger.debug(`Author ${author}`);
-        var text = reaction.message.content;
-        bot.logger.debug(`text ${text}`);
-        var embeds = reaction.message.embeds;
-        bot.logger.debug(`embeds ${embeds}`);
-        var time = reaction.message.createdTimestamp;
-        bot.logger.debug(`time ${time}`);
-
-        var result = new Discord.RichEmbed();
-
-        result.setTimestamp(time);
-        result.setColor(author.displayColor);
-        result.setAuthor(author.displayName, author.user.avatarURL);
-        result.setFooter(server.name);
-        result.setDescription(text);
-        result.setURL(reaction.message.url);
-
-        var e = reaction.message.embeds[0];
-        if (e) {
-            bot.logger.debug(`Found an embed ${e}`);
-            if (e.title) result.setTitle(e.title);
-
-            if (e.thumbnail) result.setThumbnail(e.thumbnail.url);
-
-            if (e.description)
-                result.setDescription(text + "\n\n" + e.description);
-
-            if (e.footer) result.setFooter(e.footer.text);
-
-            if (e.author) result.setAuthor(e.author.name, e.author.icon);
-
-            if (e.color) result.setColor(e.color);
-
-            if (e.image) result.setImage(e.image.url);
-
-            if (e.file) result.attachFile(e.file);
-
-            if (e.files) result.attachFiles(e.files);
-
-            if (e.url) result.setURL(e.url);
-        }
-
-        var att = reaction.message.attachments.first();
-        if (att) {
-            bot.logger.debug(`Found an attachment ${att}`);
-            result.setImage(att.url);
-        }
-
-        if (starredMsgs[reaction.message.id]) {
-            bot.logger.info(
-                `Message was starred already: ${reaction.message.url}`
-            );
-            var id = starredMsgs[reaction.message.id].star_id;
-            channel.messages
-                .find(msg => msg.id === id)
-                .edit(`${reaction.emoji} #${reaction.count}`, result);
-            return;
-        } else {
-            channel
-                .send(`${reaction.emoji} #${reaction.count}`, result)
-                .then(msg => {
-                    starredMsgs[reaction.message.id] = { star_id: msg.id };
-                });
-        }
-    }
-
-    //    bot.commands.get('reactrole').handle(reaction, user);
-
-    updateJSON(starFile, starredMsgs);
 });
 
 //logging stuff
@@ -420,31 +215,6 @@ function loadCmds(message) {
 
 function handleDM(message) {
     //do something here
-}
-
-bot.collectionToJSON = function(collection) {
-    var obj = {};
-    var keys = collection.keyArray();
-
-    for (var i = 0; i < keys.length; i++) {
-        var val = collection.get(keys[i]);
-        obj[keys[i]] = val;
-    }
-
-    bot.logger.debug("bot.collectionToJSON returned: " + obj);
-
-    return obj;
-};
-
-function JSONtoCollection(obj) {
-    var coll = new Discord.Collection();
-
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            coll.set(key, obj[key]);
-        }
-    }
-    return coll;
 }
 
 bot.login(config.token);
